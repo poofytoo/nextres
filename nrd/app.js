5248b798615ea050a5f7c0ab398a6e5b01e8b3a7
poofytoo <victorhung92@gmail.com>
2013-09-19 02:06:57 -0400

    Handlebars and more node setup

--- nrd/app.js
+++ nrd/app.js
@@ -8,26 +8,47 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var Consolidate = require('consolidate');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.engine('html', Consolidate.handlebars);
+app.set('view engine', 'html');
+
 app.use(express.favicon());
+app.use(express.cookieParser());
 app.use(express.logger('dev'));
+
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+  
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- nrd/routes/index.js
+++ nrd/routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 
+// fuck this jade shit
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
