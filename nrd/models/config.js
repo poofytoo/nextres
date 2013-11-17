0db76712b2d848b44a999dc7022f81c7aeb7bcff
al <tim@SIMPLE.MIT.EDU>
2013-11-17 13:35:30 -0500

    Directly specify encoding for config.json.

--- nrd/models/config.js
+++ nrd/models/config.js
@@ -10,6 +10,6 @@ if (!fs.existsSync(CONFIG_FILE)) {
     throw new Error("Config file config.json not found");
 }
 
-data = fs.readFileSync(CONFIG_FILE, {'encoding': 'utf8'});
+data = fs.readFileSync(CONFIG_FILE);
 json_data = JSON.parse(data);
 exports.config_data = json_data;
