1e4e0386ec696356d95a6fad06303046c7cce189
Michael Xu <michaelx@mit.edu>
2014-07-07 20:15:24 -0700

    refactoring attractive people

--- nrd/routes/shenanigans.js
+++ nrd/routes/shenanigans.js
@@ -1,13 +1,9 @@
 // req.params = {kerberos: [KERBEROS]}
-var nowTrolling = ['kyc2915', 'normandy', 'fishr', 'dontony', 'trzhang', 'hguarino', 'stalyc'];
-
 exports.view = function(req, res) {
   if (req.params.kerberos 
       && nowTrolling.indexOf(req.params.kerberos) > -1
       && req.host === 'next.sexy') {
-    var data = {};
-    data[req.params.kerberos] = true;
-    res.render('peep.html', data);
+    res.render('peep.html', {resident : req.params.kerberos});
   } else {
     res.redirect('/');
   }
