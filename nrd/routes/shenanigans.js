acf4289a6e1756082332b003760b65fb5e7b141d
Michael Xu <michaelx@mit.edu>
2014-07-07 19:47:45 -0700

    so many lovely faces

--- nrd/routes/shenanigans.js
+++ nrd/routes/shenanigans.js
@@ -1,9 +1,10 @@
 // req.params = {kerberos: [KERBEROS]}
-var nowTrolling = ['kyc2915', 'normandy', 'fishr'];
+var nowTrolling = ['kyc2915', 'normandy', 'fishr', 'dontony', 'trzhang', 'hguarino', 'stalyc'];
 
 exports.view = function(req, res) {
   if (req.params.kerberos 
-      && nowTrolling.indexOf(req.params.kerberos) > -1) {
+      && nowTrolling.indexOf(req.params.kerberos) > -1
+      && req.host === 'next.sexy') {
     var data = {};
     data[req.params.kerberos] = true;
     res.render('peep.html', data);
