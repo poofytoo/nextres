ab80a9ac832d34cadade3dccc6c945ffe0bef24c
Michael Xu <michaelx@mit.edu>
2014-07-07 20:22:02 -0700

    my bad, pretty people

--- nrd/routes/shenanigans.js
+++ nrd/routes/shenanigans.js
@@ -1,8 +1,7 @@
 // req.params = {kerberos: [KERBEROS]}
 exports.view = function(req, res) {
   if (req.params.kerberos 
-      && nowTrolling.indexOf(req.params.kerberos) > -1
-      && req.host === 'next.sexy') {
+      && (req.host === 'next.sexy' || req.host === 'localhost')) {
     res.render('peep.html', {resident : req.params.kerberos});
   } else {
     res.redirect('/');
